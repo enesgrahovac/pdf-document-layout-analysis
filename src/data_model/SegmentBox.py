393ee6283d40431d80758d7569d9924c8c08b96b
Enes Grahovac <enesxgrahovac@gmail.com>
2024-11-16 13:10:12 -0500

    small relative import for data_model

--- src/data_model/SegmentBox.py
+++ src/data_model/SegmentBox.py
@@ -1,4 +1,4 @@
-from fast_trainer import PdfSegment
+from ..fast_trainer import PdfSegment
 from pdf_features import PdfPage
 from pdf_token_type_labels import TokenType
 from pydantic import BaseModel

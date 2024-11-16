7d2213c16b2b63f8f8f92da87523bfa4bccce659
Enes Grahovac <enesxgrahovac@gmail.com>
2024-11-16 13:21:27 -0500

    small relative import for data_model

--- src/data_model/__init__.py
+++ src/data_model/__init__.py
@@ -1,6 +1,5 @@
 from .SegmentBox import SegmentBox
 from .PdfImages import PdfImages
 from .Prediction import Prediction
-from ..fast_trainer import PdfSegment
 
 __all__ = ["SegmentBox", "PdfImages", "Prediction", "PdfSegment"]

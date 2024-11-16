4b616075e475185b3063972d78cac6c14a41b09d
Enes Grahovac <enesxgrahovac@gmail.com>
2024-11-16 13:03:10 -0500

    adjusting imports for layout_analysis directory

--- src/controller.py
+++ src/controller.py
@@ -1,5 +1,4 @@
-from pdf_layout_analysis.run_pdf_layout_analysis import analyze_pdf
-from pdf_layout_analysis.run_pdf_layout_analysis_fast import analyze_pdf_fast
+from pdf_layout_analysis import analyze_pdf, analyze_pdf_fast
 from starlette.concurrency import run_in_threadpool
 
 async def analyze_pdf_controller(file_content: bytes, fast: bool, extraction_format: str):
---
+++ src/pdf_layout_analysis/__init__.py
@@ -0,0 +1,4 @@
+from .run_pdf_layout_analysis import analyze_pdf
+from .run_pdf_layout_analysis_fast import analyze_pdf_fast
+
+__all__ = ["analyze_pdf", "analyze_pdf_fast"]
